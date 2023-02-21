720ce0822de0c1b884013fa0abac8c6b589c0522
mridul764 <54048418+mridul764@users.noreply.github.com>
2023-02-21 22:38:55 +0530

    home directory to sub route

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import './directory-item.styles.css'
+import { useNavigate } from "react-router-dom";
+
+
+const DirectoryItem = ({category}) =>{
+    const{title,imageUrl,route} = category;
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route);
+     return (
+        <div className="directory-item-container" onClick={onNavigateHandler}>
+            <div className='background-image'
+                style={
+                    {
+                        backgroundImage: `url(${imageUrl})`
+                    }
+                } />
+            <div className="body">
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
+
+        </div>
+    
+    )
+
+}
+
+export default DirectoryItem;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -19,6 +19,10 @@ import{
   doc,
   getDoc,
   setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs
 
 } from 'firebase/firestore';
 
@@ -47,6 +51,21 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth,provider);
 
 export const db = getFirestore();
 
+export const addCollectionAndDocuments = async(collectionKey,objectsToAdd) => {
+  const collectionRef = collection(db,collectionKey);
+  const batch = writeBatch(db);
+  
+  objectsToAdd.forEach(object => {
+    const docRef = doc(collectionRef,object.title.toLowerCase());
+    batch.set(docRef,object);
+  });
+   
+  await batch.commit();
+  console.log('done');
+};
+
+
+
 export const createUserDocumentFromAuth = async(userAuth, additionalInformation = {}) =>{
   const userDocRef = doc(db,'users', userAuth.uid);
 
@@ -71,6 +90,19 @@ export const createUserDocumentFromAuth = async(userAuth, additionalInformation 
 
   return userDocRef;
 };
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db,'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const {title,items} = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  },{})
+  return categoryMap;
+}
 
 export const createAuthUserWithEmailAndPassword = async(email,password) =>{
   if(!email || !password) return ;
